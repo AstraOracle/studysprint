c5ca12d94390e66a41fdeb38bb09674119bc0469
AstraOracle <anubhav1nagpal@gmail.com>
2025-12-09 18:34:38 -0600

    feat(shell): add side menu, router outlet, and main pages (#1)

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,16 +2,13 @@ import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  {
-    path: 'home',
-    loadChildren: () => import('./home/home.module').then( m => m.HomePageModule)
-  },
-  {
-    path: '',
-    redirectTo: 'home',
-    pathMatch: 'full'
-  },
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', loadChildren: () => import('./pages/home/home.module').then(m => m.HomePageModule) },
+  { path: 'flashcards', loadChildren: () => import('./pages/flashcards/flashcards.module').then(m => m.FlashcardsPageModule) },
+  { path: 'session', loadChildren: () => import('./pages/session/session.module').then(m => m.SessionPageModule) },
+  { path: 'settings', loadChildren: () => import('./pages/settings/settings.module').then(m => m.SettingsPageModule) }
 ];
+
 
 @NgModule({
   imports: [
